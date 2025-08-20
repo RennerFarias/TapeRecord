35e3d00cd7ed79b94d9533679359ba60d20dd8d3
Igor Morais <igorlimamorais12@gmail.com>
2025-08-20 10:32:10 -0300

    Update Musica.java

--- Classes/Musica.java
+++ Classes/Musica.java
@@ -1,7 +1,7 @@
 public class Musica extends Midia {
 
-	public Musica(String Titulo, String Artista, double Duração, Genero genero) {
-		super(Titulo, Artista, Duração, genero);
+	public Musica(String Titulo, String Artista, double Duracao, Genero genero) {
+		super(Titulo, Artista, Duracao, genero);
 		
 	}	
 }
