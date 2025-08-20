f2ffaa114819414120d00b284b1120f6a7a7fd53
Igor Morais <igorlimamorais12@gmail.com>
2025-08-20 10:18:37 -0300

    Update podcast.java

--- Classes/podcast.java
+++ Classes/podcast.java
@@ -1,6 +1,6 @@
 public class Podcast extends Midia {
 
-	public Podcast(String Titulo, String Artista, int Duração, Genero genero) {
+	public Podcast(String Titulo, String Artista, double Duração, Genero genero) {
 		super(Titulo, Artista, Duração, genero);
 
 	}
